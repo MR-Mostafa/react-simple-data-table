a2fbc88a8cdd845024be67fe72eb1c8aace44fb3
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-24 20:05:48 +0330

    feat: add react-query provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,24 @@ import '~src/assets/styles/index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { RouterProvider } from 'react-router-dom';
 
 import { ErrorBoundary } from '~src/features';
 
 import router from './router';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<ErrorBoundary>
-			<RouterProvider router={router} />
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</ErrorBoundary>
 	</React.StrictMode>
 );
