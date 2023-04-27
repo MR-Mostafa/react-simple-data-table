0eb03df7a8140e0161a6ef358b46135a5f7b2f1d
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-27 17:23:59 +0330

    perf: separated memorized data for performance

--- src/features/productsTable/productsTable.tsx
+++ src/features/productsTable/productsTable.tsx
@@ -2,7 +2,7 @@ import { memo, useMemo, useState } from 'react';
 
 import { Table } from '~src/components';
 import { useGetAllProducts } from '~src/services';
-import { ProductsKeys } from '~src/types';
+import { type ProductItem, type ProductsKeys } from '~src/types';
 
 interface SortStateType {
 	sortBy?: string;
@@ -68,58 +68,55 @@ export const ProductsTable = memo(({ page, limit, searchBy, searchText }: Produc
 	}, [isSuccess, activeSort]);
 
 	/**
-	 * Displays a portion of the filtered and sorted products array based on the current page and limit.
+	 * 1. Sorts an array of products based on a specified sort key and sort type.
 	 **/
-	const computingData = useMemo(() => {
+	const sortProducts = useMemo<ProductItem[]>(() => {
 		const products = data?.products ? [...data.products] : [];
 
 		if (!products || products.length === 0) return [];
 
 		const { sortBy, sortType } = activeSort;
 
-		/**
-		 * sorts an array of products based on a specified sort key and sort type.
-		 */
-		const sortProducts = () => {
-			if (!sortBy || !sortType) return products;
+		if (!sortBy || !sortType) return products;
 
-			return products.sort((a, b) => {
-				const valA = a[sortBy as ProductsKeys];
-				const valB = b[sortBy as ProductsKeys];
+		return products.sort((a, b) => {
+			const valA = a[sortBy as ProductsKeys];
+			const valB = b[sortBy as ProductsKeys];
 
-				if (typeof valA === 'number' && typeof valB === 'number') {
-					return activeSort.sortType === 'asc' ? valA - valB : valB - valA;
-				}
+			if (typeof valA === 'number' && typeof valB === 'number') {
+				return activeSort.sortType === 'asc' ? valA - valB : valB - valA;
+			}
 
-				const options = { sensitivity: 'base', usage: 'sort', ignorePunctuation: true };
+			const options = { sensitivity: 'base', usage: 'sort', ignorePunctuation: true };
 
-				return activeSort.sortType === 'asc'
-					? valA.toString().localeCompare(valB.toString(), undefined, options)
-					: valB.toString().localeCompare(valA.toString(), undefined, options);
-			});
-		};
+			return activeSort.sortType === 'asc'
+				? valA.toString().localeCompare(valB.toString(), undefined, options)
+				: valB.toString().localeCompare(valA.toString(), undefined, options);
+		});
+	}, [activeSort, data]);
 
-		/**
-		 * filters products based on a search query.
-		 */
-		const filterProducts = () => {
-			if (!searchBy || !searchText) return sortProducts();
+	/**
+	 * 2. Filters products based on a search query.
+	 */
+	const filterProducts = useMemo<ProductItem[]>(() => {
+		if (!searchBy || !searchText) return sortProducts;
 
-			return sortProducts().filter((item) => {
-				const columnItem = item[searchBy].toString().trim().toLowerCase();
+		return sortProducts.filter((item) => {
+			const columnItem = item[searchBy].toString().trim().toLowerCase();
 
-				return columnItem.includes(searchText);
-			});
-		};
+			return columnItem.includes(searchText);
+		});
+	}, [sortProducts, searchText]);
 
+	/**
+	 * 3. Calculates the portion of the filtered and sorted products array to be displayed based on the current page and limit.
+	 **/
+	const paginationProducts = useMemo<ProductItem[]>(() => {
 		const start = (page - 1) * limit;
 		const end = limit * page;
 
-		/**
-		 * Returning a portion of the filtered and sorted products array based on the current page and limit
-		 */
-		return filterProducts().slice(start, end);
-	}, [activeSort, data, searchText, page, limit]);
+		return filterProducts.slice(start, end);
+	}, [filterProducts, page, limit]);
 
 	return (
 		<Table isTruncated>
@@ -130,11 +127,11 @@ export const ProductsTable = memo(({ page, limit, searchBy, searchText }: Produc
 
 					{isError && <Table.Error tdProps={{ colSpan: 6 }} />}
 
-					{isSuccess && computingData.length === 0 && <Table.Empty tdProps={{ colSpan: 6 }} />}
+					{isSuccess && paginationProducts.length === 0 && <Table.Empty tdProps={{ colSpan: 6 }} />}
 
 					{isSuccess &&
-						computingData.length !== 0 &&
-						computingData.map((item) => {
+						paginationProducts.length !== 0 &&
+						paginationProducts.map((item) => {
 							return (
 								<tr key={item.id}>
 									<td className="text-center">{new Intl.NumberFormat('en-GB', { style: 'decimal' }).format(item.id)}</td>
