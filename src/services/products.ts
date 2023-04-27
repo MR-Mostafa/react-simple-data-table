0c362405ee6be3742b1264b6f2220fcb4e52311d
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-27 16:35:35 +0330

    fix: get all products by once fetch api

--- src/services/products.ts
+++ src/services/products.ts
@@ -4,22 +4,13 @@ import { ProductList } from '~src/types';
 
 import { getFetcher } from './api';
 
-interface GetProductsProps {
-	page: number;
-	limit: number;
-}
-
-export const useGetAllProducts = ({ page, limit }: GetProductsProps) => {
-	const pageNumber = Math.abs(page) || 1;
-	const limitNumber = Math.abs(limit) || 30;
-	const skip = (pageNumber - 1) * limitNumber;
-
-	const key = ['products', { page, limit: limitNumber }];
+export const useGetAllProducts = () => {
+	const key = ['products'];
 
 	return useQuery<ProductList>(
 		key,
 		async () => {
-			const { data } = await getFetcher<ProductList>(`/products?limit=${limitNumber}&skip=${skip}`);
+			const { data } = await getFetcher<ProductList>(`/products?limit=100`);
 
 			return data;
 		},
