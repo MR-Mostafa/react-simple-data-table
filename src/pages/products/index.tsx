8e2b5c923666e6290686977aee3e39f21ce1cf7e
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-27 05:14:12 +0330

    perf: add useDeferredValue hook

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useDeferredValue, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -16,15 +16,32 @@ const Products = () => {
 	const [page] = useState(1);
 	const [inputs, setInputs] = useState<InputsStateType>({ limit: 30, searchText: '', searchBy: 'title' });
 
+	const deferredInputs = useDeferredValue(inputs);
+
 	return (
 		<Container className="py-5">
 			<Row>
 				<Col xs={12}>
-					<ProductsHeader values={inputs} setValues={setInputs} />
+					<ProductsHeader
+						values={inputs}
+						setValues={({ name, value }) => {
+							setInputs((prev) => {
+								return {
+									...prev,
+									[name]: Number.isNaN(+value) ? value.toLowerCase() : +value,
+								};
+							});
+						}}
+					/>
 				</Col>
 
 				<Col xs={12}>
-					<ProductsTable page={page} limit={inputs.limit} searchBy={inputs.searchBy} searchText={inputs.searchText} />
+					<ProductsTable
+						page={page}
+						limit={deferredInputs.limit}
+						searchBy={deferredInputs.searchBy}
+						searchText={deferredInputs.searchText}
+					/>
 				</Col>
 			</Row>
 		</Container>
