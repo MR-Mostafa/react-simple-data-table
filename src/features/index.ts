bb0c3566583597912ec46e74e7ba19471721c898
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-27 05:13:40 +0330

    feat: create product header feature

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,2 +1,3 @@
 export { ErrorBoundary } from './errorFallback/errorBoundary';
+export { ProductsHeader } from './productsTable/productsHeader';
 export { ProductsTable } from './productsTable/productsTable';
