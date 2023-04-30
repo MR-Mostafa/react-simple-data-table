0b68027eb1a49e41eb839a84ecd79b0256994542
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-30 19:34:17 +0330

    feat: add loader in router

--- src/router.tsx
+++ src/router.tsx
@@ -1,37 +1,43 @@
 import { lazy as ReactLazy, Suspense as ReactSuspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-import { LoadingSpinner, ScrollTop } from '~src/components';
+import { type QueryClient } from '@tanstack/react-query';
+
+import { LoadingSpinner, RedirectTo, ScrollTop } from '~src/components';
 import NotFound from '~src/pages/404';
+import { getProductByIDLoader } from '~src/services';
 
 const ProductsPage = ReactLazy(() => import('~src/pages/products'));
 const ProductDetails = ReactLazy(() => import('~src/pages/products/[productId]'));
 
-const router = createBrowserRouter([
-	{
-		path: '/products',
-		element: (
-			<ReactSuspense fallback={<LoadingSpinner />}>
-				<ScrollTop>
-					<ProductsPage />
-				</ScrollTop>
-			</ReactSuspense>
-		),
-	},
-	{
-		path: '/products/:id',
-		element: (
-			<ReactSuspense fallback={<LoadingSpinner />}>
-				<ScrollTop>
-					<ProductDetails />
-				</ScrollTop>
-			</ReactSuspense>
-		),
-	},
-	{
-		path: '*',
-		element: <NotFound />,
-	},
-]);
+const router = (queryClient: QueryClient) =>
+	createBrowserRouter([
+		{
+			path: '/products',
+			element: (
+				<ReactSuspense fallback={<LoadingSpinner />}>
+					<ScrollTop>
+						<ProductsPage />
+					</ScrollTop>
+				</ReactSuspense>
+			),
+		},
+		{
+			path: '/products/:productId',
+			element: (
+				<ReactSuspense fallback={<LoadingSpinner />}>
+					<ScrollTop>
+						<ProductDetails />
+					</ScrollTop>
+				</ReactSuspense>
+			),
+			errorElement: <RedirectTo to="/404" />,
+			loader: getProductByIDLoader(queryClient), //
+		},
+		{
+			path: '*',
+			element: <NotFound />,
+		},
+	]);
 
 export default router;
